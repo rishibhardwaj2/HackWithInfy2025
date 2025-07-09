244e3ea5c1f8ac2ce31f070d938a345ec4bdc613
Rishi bhardwaj <rishibhardwaj170@gmail.com>
2025-07-09 09:54:07 +0530

    lcs problem solution

---
+++ DynamicProgramming/LongestCommonSubsequence.java
@@ -0,0 +1,32 @@
+public class LongestCommonSubsequence {
+
+    public int longestCommonSubsequence(String text1, String text2) {
+        int m = text1.length();
+        int n = text2.length();
+
+        // Create a DP table to store lengths of longest common subsequence
+        int[][] dp = new int[m + 1][n + 1];
+
+        // Fill the DP table
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1; // Characters match
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]); // Take max from left or top
+                }
+            }
+        }
+
+        return dp[m][n]; // Length of longest common subsequence
+    }
+
+    // Optional: main method to test
+    public static void main(String[] args) {
+        LongestCommonSubsequence lcs = new LongestCommonSubsequence();
+        String text1 = "abcde";
+        String text2 = "ace";
+        int length = lcs.longestCommonSubsequence(text1, text2);
+        System.out.println("Length of Longest Common Subsequence: " + length);
+    }
+}
